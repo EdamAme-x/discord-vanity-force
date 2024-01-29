716e08e68e640a1cf669fa23ded9611ca8af4915
EdamAme <121654029+EdamAme-x@users.noreply.github.com>
2024-01-29 23:50:00 +0900

    fix

--- main.ts
+++ main.ts
@@ -18,7 +18,16 @@ if (isNaN(length) || length < 2) {
     Deno.exit(1);
 }
 
-Logger.log("info", "INFO", "Start | Exit with CTRL + C");
+Logger.log("info", "MODE", `1. Safe Mode | 2. Fast Mode (Unsafe)`);
+
+const mode = prompt("Mode : ") ?? "1";
+
+if (mode !== "1" && mode !== "2") {
+    Logger.log("error", "ERROR", "Invalid mode");
+    Deno.exit(1);
+}
+
+Logger.log("info", "INFO", "Start mode : " + (mode === "1" ? "Safe Mode" : "Fast Mode") + " | Exit with CTRL + C");
 
 const cachedCode = new Set<string>();
 
